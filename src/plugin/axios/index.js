d1d297d290287444c7f2bcd3afcf924e0ed98ddc
anlingyi <1090172196@qq.com>
2020-04-20 11:10:11 +0800

    优化响应拦截器配置

--- src/plugin/axios/index.js
+++ src/plugin/axios/index.js
@@ -2,25 +2,27 @@ import axios from 'axios'
 import vue from 'vue'
 
 const api_url = process.env.VUE_APP_API_URL
+const debug = process.env.VUE_APP_DEBUG
 
 const service = axios.create({
     baseURL: api_url,
     timeout: 5000
 })
 
+function addLog(log) {
+    if (debug === 'true') {
+        console.log(log)
+    }
+}
+
 // 响应拦截器
 service.interceptors.response.use((resp) => {
-    if (process.env.NODE_ENV === 'development') {
-        console.log(resp)
-    }
-
-    if (resp.data.code === 200) {
-        // 请求成功
-        return resp.data
-    }
+    addLog(resp)
+    // 请求成功
+    return resp.data
 }, (error) => {
-    // 请求失败
     vue.prototype.$toast.error('网络错误！')
+    return Promise.reject(error)
 })
 
 export default service
