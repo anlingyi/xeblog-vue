bd9bbfcb7856a6f9f3751c347c202be9574fabba
anlingyi <1090172196@qq.com>
2020-11-23 03:03:52 +0800

    GPS to AMap

--- src/views/footprint/index.vue
+++ src/views/footprint/index.vue
@@ -106,7 +106,7 @@ export default {
               this.$toast.error(msg, 6000)
             },
             {
-              maximumAge: 15000,
+              maximumAge: 0,
               timeout: 10000,
               enableHighAccuracy: true
             }
@@ -118,7 +118,7 @@ export default {
     getAddress(lng, lat) {
       axios.get('https://restapi.amap.com/v3/geocode/regeo', {
         params: {
-          location: lng + ',' + lat,
+          location: this.gpsToAMap(lng, lat),
           key: '164fdccf3c6a8341f23e5d903642391f'
         }
       }).then(resp => {
@@ -134,6 +134,46 @@ export default {
         console.log(error)
         this.$toast.error("获取位置信息失败！")
       })
+    },
+    gpsToAMap(lng, lat) {
+      const PI = 3.1415926535897932384626
+      const a = 6378245.0
+      const ee = 0.00669342162296594323
+
+      function transfLng(lng, lat) {
+        let ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + 0.1 * lng * lat + 0.1 * Math.sqrt(Math.abs(lng))
+        ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+        ret += (20.0 * Math.sin(lng * PI) + 40.0 * Math.sin(lng / 3.0 * PI)) * 2.0 / 3.0
+        ret += (150.0 * Math.sin(lng / 12.0 * PI) + 300.0 * Math.sin(lng / 30.0 * PI)) * 2.0 / 3.0
+        return ret
+      }
+
+      function transfLat(lng, lat) {
+        let ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + 0.1 * lng * lat + 0.2 * Math.sqrt(Math.abs(lng))
+        ret += (20.0 * Math.sin(6.0 * lng * PI) + 20.0 * Math.sin(2.0 * lng * PI)) * 2.0 / 3.0
+        ret += (20.0 * Math.sin(lat * PI) + 40.0 * Math.sin(lat / 3.0 * PI)) * 2.0 / 3.0
+        ret += (160.0 * Math.sin(lat / 12.0 * PI) + 320 * Math.sin(lat * PI / 30.0)) * 2.0 / 3.0
+        return ret
+      }
+
+      function notInChina(lng, lat) {
+        return (lng < 72.004 || lng > 137.8347) || ((lat < 0.8293 || lat > 55.8271) || false)
+      }
+
+      if (notInChina(lng, lat)) {
+        return lng + ',' + lat
+      }
+
+      let dLat = transfLat(lng - 105.0, lat - 35.0)
+      let dLng = transfLng(lng - 105.0, lat - 35.0)
+      let radLat = lat / 180.0 * PI
+      let magic = 1 - ee * Math.pow(Math.sin(radLat), 2)
+      let sqrtMagic = Math.sqrt(magic);
+      dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * PI)
+      dLng = (dLng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * PI)
+      let mgLat = lat + dLat
+      let mgLng = lng + dLng
+      return mgLng + ',' + mgLat
     }
   },
   mounted() {
