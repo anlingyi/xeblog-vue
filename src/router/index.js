69178a0b6cea6ec08aacf650d3e502b57ee09e1f
anlingyi <1090172196@qq.com>
2021-05-18 04:45:04 +0800

    填充

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,8 @@ router.beforeEach((to, from, next) => {
     let websiteInfo = Vue.prototype.$websiteInfo
     if (!websiteInfo) {
         websiteInfo = {
-            title: 'title',
-            subtitle: 'subtitle'
+            title: '小毅博客',
+            subtitle: '分享的不只是代码更是生活！'
         }
     }
     document.title = (to.meta.title || 'xeblog') + ' | ' + websiteInfo.title + '-' + websiteInfo.subtitle
